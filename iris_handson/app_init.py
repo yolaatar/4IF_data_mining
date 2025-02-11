3cbfa21de3c2b0125aa66f0e775a6f89b6c69798
yolaatar <laatarjo@gmail.com>
2025-02-11 16:53:49 +0100

    des ajouts sur les mots mais uqi sont pas bien finis

---
+++ iris_handson/app_init.py
@@ -0,0 +1,14 @@
+# import the library to create web app
+import streamlit as st 
+
+# add a title to the main page
+st.title("Clustering IRIS dataset")
+
+# to run the app, run the command "streamlit run app_init.py" in the terminal
+
+# add markdown
+# st.markdown("This project is an application of clustering techniques to the IRIS dataset")
+
+# save the changes
+# in the app, you can click on "Always rerun" to apply changes automatically
+
---
+++ iris_handson/src/pages/home.py
@@ -0,0 +1,3 @@
+import streamlit as st 
+
+st.title("Clustering IRIS dataset")
